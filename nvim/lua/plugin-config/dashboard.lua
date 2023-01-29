d0cdc4a501f9c29b434b5745f9bb027f61cddce5
jiaxinaoliao <17600930534@163.com>
2023-01-29 12:22:20 +0800

    22.1.29/12

--- nvim/lua/plugin-config/dashboard.lua
+++ nvim/lua/plugin-config/dashboard.lua
@@ -1,62 +1,84 @@
 local status, db = pcall(require, 'dashboard')
 if not status then
-    vim.notify('没有找到 dashboard')
-    return
+  vim.notify('没有找到 dashboard')
+  return
 end
 
-db.custom_footer = {
-    '',
-    '',
-    'https://github.com/jiaxinaoliao',
-}
+db.setup {
+  theme = 'doom',
+  config = {
+    footer = {
+      '',
+      '',
+      'https://github.com/jiaxinaoliao',
+    },
 
-db.custom_center = {
-    {
-        icon = '      ',
+    center = {
+      {
+        icon = '                     ',
         desc = 'Find file                           ',
         action = 'Telescope find_files',
-    },
-    -- {
-    --   icon = "  ",
-    --   desc = "Projects                            ",
-    --   action = "Telescope projects",
-    -- },
-    -- {
-    --   icon = "  ",
-    --   desc = "Recently files                      ",
-    --   action = "Telescope oldfiles",
-    -- },
-    {
-        icon = '      ',
+      },
+      -- {
+      --   icon = "  ",
+      --   desc = "Projects                            ",
+      --   action = "Telescope projects",
+      -- },
+      -- {
+      --   icon = "  ",
+      --   desc = "Recently files                      ",
+      --   action = "Telescope oldfiles",
+      -- },
+      {
+        icon = '                     ',
         desc = 'Edit keybindings                    ',
         action = 'edit ~/AppData/local/nvim/lua/keybindings.lua',
-    },
-    -- {
-    --   icon = "  ",
-    --   desc = "Edit Projects                       ",
-    --   action = "edit ~/.local/share/nvim/project_nvim/project_history",
-    -- },
-    {
-        icon = '      ',
+      },
+      -- {
+      --   icon = "  ",
+      --   desc = "Edit Projects                       ",
+      --   action = "edit ~/.local/share/nvim/project_nvim/project_history",
+      -- },
+      {
+        icon = '                     ',
         desc = 'Edit PowerShell                     ',
         action = 'edit D:/UsersData/Documents/PowerShell/Microsoft.PowerShell_profile.ps1',
+      },
+      -- {
+      --   icon = '            ',
+      --   desc = 'Change colorscheme                  ',
+      --   action = ':ColorScheme',
+      -- },
+      {
+        icon = '                     ',
+        desc = "Edit init.lua                       ",
+        action = 'edit ~/AppData/local/nvim/init.lua',
+      },
+      -- {
+      --   icon = "  ",
+      --   desc = "Find text                           ",
+      --   action = "Telescopecope live_grep",
+      -- },
     },
-    {
-        icon = '      ',
-        desc = 'Change colorscheme                  ',
-        action = ':ColorScheme',
+
+    header = {
+      [[]],
+      [[                                              ]],
+      [[       ██╗██╗ █████╗        █████╗  ██████╗   ]],
+      [[       ██║██║██╔══██╗      ██╔══██╗██╔═══██╗  ]],
+      [[       ██║██║███████║      ███████║██║   ██║  ]],
+      [[  ██   ██║██║██╔══██║      ██╔══██║██║   ██║  ]],
+      [[  ╚█████╔╝██║██║  ██║      ██║  ██║╚██████╔╝  ]],
+      [[   ╚════╝ ╚═╝╚═╝  ╚═╝      ╚═╝  ╚═╝ ╚═════╝   ]],
+      [[                                              ]],
+      [[                                              ]],
+      [[              [ version : 5.2.1 ]             ]],
+      [[]],
+      [[]],
     },
-    -- {
-    --   icon = "  ",
-    --   desc = "Edit init.lua                       ",
-    --   action = "edit ~/.config/nvim/init.lua",
-    -- },
-    -- {
-    --   icon = "  ",
-    --   desc = "Find text                           ",
-    --   action = "Telescopecope live_grep",
-    -- },
+  }
 }
+
 -- db.custom_header = {
 --     [[]],
 --     [[███╗   ██╗███████╗██╗  ██╗███████╗███╗   ██╗]],
@@ -91,16 +113,3 @@ db.custom_center = {
 --     [[                                                   ]],
 --     [[                [ version : 1.0.0 ]                ]],
 -- }
-
-db.custom_header = {
-    [[]],
-    [[                                              ]],
-    [[       ██╗██╗ █████╗        █████╗  ██████╗   ]],
-    [[       ██║██║██╔══██╗      ██╔══██╗██╔═══██╗  ]],
-    [[       ██║██║███████║      ███████║██║   ██║  ]],
-    [[  ██   ██║██║██╔══██║      ██╔══██║██║   ██║  ]],
-    [[  ╚█████╔╝██║██║  ██║      ██║  ██║╚██████╔╝  ]],
-    [[   ╚════╝ ╚═╝╚═╝  ╚═╝      ╚═╝  ╚═╝ ╚═════╝   ]],
-    [[                                              ]],
-    [[              [ version : 5.2.1 ]             ]],
-}
