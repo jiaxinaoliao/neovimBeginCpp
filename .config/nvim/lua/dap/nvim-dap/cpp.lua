8f4d65da2725ad64ff309cc1d8e782618b712f0e
jiaxinaoliao <jiaxinaoliao163@163.com>
2023-04-23 19:18:27 +0800

    23.04.23/18

--- .config/nvim/lua/dap/nvim-dap/cpp.lua
+++ .config/nvim/lua/dap/nvim-dap/cpp.lua
@@ -1,8 +1,9 @@
 local dap = require('dap')
+
 dap.adapters.cppdbg = {
   id = 'cppdbg',
   type = 'executable',
-  command = "/home/jia/.local/share/nvim/mason/bin/OpenDebugAD7",
+  command = "/home/jia/data/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
 }
 
 dap.configurations.cpp = {
@@ -13,11 +14,30 @@ dap.configurations.cpp = {
     program = function()
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
-
     cwd = '${workspaceFolder}',
     stopAtEntry = true,
   },
+  -- {
+  --   name = 'Attach to gdbserver :1234',
+  --   type = 'cppdbg',
+  --   request = 'launch',
+  --   MIMode = 'gdb',
+  --   miDebuggerServerAddress = 'localhost:1234',
+  --   --miDebuggerPath = '/usr/bin/gdb',
+  --   cwd = '${workspaceFolder}',
+  --   program = function()
+  --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  --   end,
+  -- },
 }
 
--- setup other language
+setupCommands = {  
+  { 
+     text = '-enable-pretty-printing',
+     description =  'enable pretty printing',
+     ignoreFailures = false 
+  },
+}
+
 dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
